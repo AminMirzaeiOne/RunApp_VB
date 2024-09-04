1690a05af831986489800c23d06e1108ca48d8e4
Amin Mirzaei <aminmirzaei.og@gmail.com>
2024-09-04 04:04:45 +0330

    - Update TextBox Control

--- RunApp/Controls/Textbox.vb
+++ RunApp/Controls/Textbox.vb
@@ -1,3 +1,14 @@
 ﻿Public Class TextBox
+    Public Sub New(left As Integer, top As Integer, width As Integer, height As Integer)
+
+    End Sub
+
+    Public Property BackColor As ConsoleColor = ConsoleColor.Black
+    Public Property ForeColor As ConsoleColor = ConsoleColor.White
+    Public Property BorderColor As ConsoleColor = ConsoleColor.Blue
+    Public Property HolderColor As ConsoleColor = ConsoleColor.Gray
+    Public Property HolderText As String = "Enter Text"
+
+
 
 End Class
