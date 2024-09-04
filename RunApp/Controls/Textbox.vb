60751d03cda096c89e6396235f6694c7d01a7328
Amin Mirzaei <aminmirzaei.og@gmail.com>
2024-09-04 05:13:17 +0330

    - Update TextBox Control

--- RunApp/Controls/Textbox.vb
+++ RunApp/Controls/Textbox.vb
@@ -1,5 +1,60 @@
-﻿Public Class TextBox
+﻿Imports System.ComponentModel
+
+Public Enum Aligns
+    Left
+    Center
+    Right
+End Enum
+
+Public Class TextBox
+
     Public Sub New(left As Integer, top As Integer, width As Integer, height As Integer)
+        Console.SetBufferSize(Console.BufferWidth, Console.BufferHeight + 100)
+        Console.SetCursorPosition(left, top)
+        Dim wleft As Integer = 0
+        Dim hleft As Integer = 0
+        Console.ForegroundColor = BorderColor
+        Console.BackgroundColor = ConsoleColor.White
+        Console.Write("╔")
+        For index = 0 To width
+            Console.Write("═")
+            If index = width Then
+                wleft = Console.CursorLeft
+                Console.Write("╗")
+            End If
+        Next
+
+        Console.SetCursorPosition(left, top)
+        Dim y As Integer = top
+        For index = 0 To height
+            y = y + 1
+            Console.SetCursorPosition(left, y)
+            Console.Write("║")
+            If index = height Then
+                hleft = Console.CursorTop
+            End If
+        Next
+
+        Console.SetCursorPosition(wleft, top)
+        Dim y2 As Integer = top
+        For index = 0 To height
+            y2 = y2 + 1
+            Console.SetCursorPosition(wleft, y2)
+            Console.Write("║")
+        Next
+
+        Console.SetCursorPosition(left, hleft)
+        Console.Write("╚")
+        For index = 0 To width
+            Console.Write("═")
+            If index = width Then
+                Console.Write("╝")
+            End If
+        Next
+
+    End Sub
+
+    Public Sub EnterText()
 
     End Sub
 
@@ -8,6 +63,7 @@
     Public Property BorderColor As ConsoleColor = ConsoleColor.Blue
     Public Property HolderColor As ConsoleColor = ConsoleColor.Gray
     Public Property HolderText As String = "Enter Text"
+
 
 
 
