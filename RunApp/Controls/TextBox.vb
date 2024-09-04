10656dd57120f046a5c78529700fb6f120b478a5
Amin Mirzaei <aminmirzaei.og@gmail.com>
2024-09-04 04:02:35 +0330

    - Create Appearance Propertis

--- RunApp/Controls/TextBox.vb
+++ RunApp/Controls/TextBox.vb
@@ -2,4 +2,9 @@
     Public Sub New(left As Integer, top As Integer, width As Integer, height As Integer)
 
     End Sub
+
+    Public Property BackColor As ConsoleColor = ConsoleColor.Black
+    Public Property ForeColor As ConsoleColor = ConsoleColor.White
+    Public Property BorderColor As ConsoleColor = ConsoleColor.Blue
+
 End Class
