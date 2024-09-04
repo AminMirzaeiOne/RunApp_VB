e03993867d9caea6eada814be2d6d56a0c440ba7
Amin Mirzaei <aminmirzaei.og@gmail.com>
2024-09-04 04:14:05 +0330

    - Update Constractor TextBox Control

--- RunApp/Controls/TextBox.vb
+++ RunApp/Controls/TextBox.vb
@@ -1,6 +1,13 @@
 ﻿Public Class TextBox
     Public Sub New(left As Integer, top As Integer, width As Integer, height As Integer)
-
+        Console.SetCursorPosition(left, top)
+        Console.Write("╔")
+        For index = 0 To width
+            Console.Write("═")
+            If index = width Then
+                Console.Write("╗")
+            End If
+        Next
     End Sub
 
     Public Property BackColor As ConsoleColor = ConsoleColor.Black
@@ -8,6 +15,7 @@
     Public Property BorderColor As ConsoleColor = ConsoleColor.Blue
     Public Property HolderColor As ConsoleColor = ConsoleColor.Gray
     Public Property HolderText As String = "Enter Text"
+
 
 
 
--- RunApp/Program.vb
+++ RunApp/Program.vb
@@ -4,7 +4,8 @@ Imports RunLibrary
 Module Program
 
     Sub Main()
-        Menu.DrawAll()
+        'Menu.DrawAll()
+        Dim txt As TextBox = New TextBox(20, 20, 20, 12)
         'Dim r As Run = New Run()
         'r.SelectApp = Run.Apps.Settings
         Console.ReadKey()
